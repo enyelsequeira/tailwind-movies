70e6adc4646731dce62bb988201b2d6846b4bf6d
EnyelSequeira <enyelsequeira@hotmail.com>
2023-04-18 15:56:00 +0100

    feat: added click outside and clear selection

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -2,20 +2,29 @@
 
 import useDebounce from "@/hooks/useDebouncedState";
 import useSearch from "@/hooks/useSeach";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { match } from "ts-pattern";
-import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
+import { XMarkIcon } from "@heroicons/react/20/solid";
 import Link from "next/link";
+import { useOnClickOutside } from "@/hooks/ useOnClickOutside";
 
 const SearchInput = () => {
   const [value, setValue] = useState<string>();
 
   const debouncedValue = useDebounce(value, 500);
   const { searchedMovies } = useSearch(debouncedValue as string);
+  const ref = useRef(null);
 
   const handleSelectItem = () => {
     setValue("");
   };
+
+  const handleClickOutside = () => {
+    setValue("");
+  };
+
+  useOnClickOutside(ref, handleClickOutside);
+
   return (
     <div className="relative flex flex-1 max-w-full  mt-2">
       <div className="relative mt-1 w-full">
@@ -26,58 +35,67 @@ const SearchInput = () => {
             value={value}
             placeholder="Search..."
           />
-          <button className="absolute inset-y-0 right-0 flex items-center pr-2">
-            <MagnifyingGlassIcon
-              className="h-5 w-5 text-gray-400"
-              aria-hidden="true"
-            />
-          </button>
+          {value && value?.length > 0 && (
+            <button
+              className="absolute inset-y-0 right-0 flex items-center pr-2"
+              onClick={() => setValue("")}
+            >
+              <XMarkIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+            </button>
+          )}
         </div>
-        {value && value?.length > 0 && (
-          <ul className="absolute mt-2  max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm space-y-2 flex flex-col">
-            {searchedMovies?.results ? (
-              <>
-                {searchedMovies?.results?.map((movie, i) => {
-                  return (
-                    <li
-                      key={i}
-                      value={i}
-                      className={"px-2 text-base  text-black font-body py-1"}
-                      onClick={handleSelectItem}
-                    >
-                      {match(movie)
-                        .with({ media_type: "tv" }, () => (
-                          <Link href={`/shows/${movie.id}`}>{movie.name}</Link>
-                        ))
-                        .with({ media_type: "movie" }, () => (
-                          <Link href={`/movies/${movie.id}`}>
-                            {movie.original_title}
-                          </Link>
-                        ))
-                        .with({ media_type: "person" }, () => (
-                          <Link
-                            href={{
-                              pathname: "/cast",
-                              query: { id: movie.id },
-                            }}
-                          >
-                            {movie.name}
-                          </Link>
-                        ))
-                        .otherwise(() => (
-                          <p>Nothing was found </p>
-                        ))}
-                    </li>
-                  );
-                })}
-              </>
-            ) : (
-              <div className="text-center py-2 font-bold font-title">
-                NO MOVIES TRY AGAIN
-              </div>
-            )}
-          </ul>
-        )}
+        <>
+          {value && value?.length > 0 && (
+            <ul
+              ref={ref}
+              className="absolute mt-2  max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm space-y-2 flex flex-col"
+            >
+              {searchedMovies?.results ? (
+                <>
+                  {searchedMovies?.results?.map((movie, i) => {
+                    return (
+                      <li
+                        key={i}
+                        value={i}
+                        className={"px-2 text-base  text-black font-body py-1"}
+                        onClick={handleSelectItem}
+                      >
+                        {match(movie)
+                          .with({ media_type: "tv" }, () => (
+                            <Link href={`/shows/${movie.id}`}>
+                              {movie.name}
+                            </Link>
+                          ))
+                          .with({ media_type: "movie" }, () => (
+                            <Link href={`/movies/${movie.id}`}>
+                              {movie.original_title}
+                            </Link>
+                          ))
+                          .with({ media_type: "person" }, () => (
+                            <Link
+                              href={{
+                                pathname: "/cast",
+                                query: { id: movie.id },
+                              }}
+                            >
+                              {movie.name}
+                            </Link>
+                          ))
+                          .otherwise(() => (
+                            <p>Nothing was found </p>
+                          ))}
+                      </li>
+                    );
+                  })}
+                </>
+              ) : (
+                <div className="text-center py-2 font-bold font-title">
+                  NO MOVIES TRY AGAIN
+                </div>
+              )}
+            </ul>
+          )}
+        </>
       </div>
     </div>
   );
